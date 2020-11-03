14478714b3d8c9c143ca5c04d76431dc304b0029
Bronagh Gaillard <bronaghgaillard@bronaghs-air.home>
2020-11-03 12:10:54 +0000

    Updates

---
+++ precache-manifest.90b90dac4fed5b3ed17404c6c0ffd6fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a491d6f61329f7025c6c584aa044f696",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9385235602b598fd7c0d",
+    "url": "./static/css/main.f701e689.chunk.css"
+  },
+  {
+    "revision": "1b3e6516d85329354c74",
+    "url": "./static/js/2.5c1f43db.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "./static/js/2.5c1f43db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9385235602b598fd7c0d",
+    "url": "./static/js/main.dd6be9ec.chunk.js"
+  },
+  {
+    "revision": "7e2adbba3c6930ec6af7",
+    "url": "./static/js/runtime-main.34384c20.js"
+  },
+  {
+    "revision": "646578fa3c0e2af916637c73b020a83b",
+    "url": "./static/media/logo2.646578fa.png"
+  }
+]);
