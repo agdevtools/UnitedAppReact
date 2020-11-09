19c37b06c2e1f801697beb6497960e920b09c450
Bronagh Gaillard <bronaghgaillard@bronaghs-air.home>
2020-11-09 19:09:11 +0000

    homepage goes directly to playerlist

--- src/component/HomePage.jsx
+++ src/component/HomePage.jsx
@@ -7,7 +7,7 @@ class Homepage extends Component {
     render() {
         return (
               <div class="header-img">
-                 <h2a>The United App <Link to="/team"> <h2b>  Click to Enter </h2b> </Link> </h2a>
+                 <h2a>The United App <Link to="/playerlist"> <h2b>  Click to Enter </h2b> </Link> </h2a>
                   </div>
         )
     }
