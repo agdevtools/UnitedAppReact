afff53c59b05041d17a16b1c4f84e275256bcf61
Armand Gaillard <armandgaillard@armands-air.home>
2020-11-27 23:40:03 +0000

    adding error handling

--- src/component/PlayerComponent.jsx
+++ src/component/PlayerComponent.jsx
@@ -11,7 +11,9 @@ class PlayerComponent extends Component {
 
         this.state = {
             playerId: this.props.match.params.id,
-            playerName: ''
+            playerName: '',
+            errors: "",
+            message: null
         }
            this.onSubmit = this.onSubmit.bind(this)
            this.validate = this.validate.bind(this)
@@ -32,14 +34,23 @@ class PlayerComponent extends Component {
 
     onSubmit(values) {
 
+        this.setState({errors: ""});
+        this.setState({message: null});
         let player = {
             playerId: values.playerId,
             playerName: values.playerName
         }
 
         if (this.props.match.params.id == -1) {
-              TeamDataService.createPlayer(player.playerId, player.playerName, player)
-                .then(() => this.props.history.push('/team'))
+                 console.log("calling team service ");
+                 TeamDataService.createPlayer(player.playerId, player.playerName, player).then(r => {
+                 console.log("response is ",r);
+                 return r.team
+                 })
+                 .catch( error => {
+                 const response = error.response
+                 console.log("this is the errors from unitedapp ", response.data.errorDetails)
+                 this.validate(response.data.errorDetails)})
         } else {
               player.playerId = this.props.match.params.id
               TeamDataService.updatePlayer(player.playerId, player.playerName, player)
@@ -48,16 +59,31 @@ class PlayerComponent extends Component {
     }
 
 
-validate(values) {
+    validate(values) {
+
+    this.setState({errors: ""});
+    console.log("These are the values passed in ", values);
     let errors = {}
-    if (!values.playerName) {
-        errors.playerName = 'Enter a Player Name'
-    } else if (values.playerName.length < 3) {
-        errors.playerName = 'Enter at least 3 Characters in Player Name'
+
+    for (let i=0; i < values.length; i++)
+    {
+    console.log("Target from error ", values[i].target);
+    if (values[i].target === "Player Id") {
+        console.log("turning on errors for ",values[i].target)
+        errors.playerId = true;
+        errors.playerId = values[i].message;
+        }
+   if (values[i].target === "Player Name") {
+        errors.playerName = true;
+        errors.playerName = values[i].message;
     }
 
+      this.setState({errors: errors});
+
+    }
     return errors
-}
+
+    }
 
     render() {
 
@@ -79,14 +105,30 @@ validate(values) {
                         {
                             (props) => (
                                 <Form>
-                                    <ErrorMessage name="playerName" component="div" className="alert alert-warning" />
+
                                     <fieldset className="form-group">
                                         <label>playerId</label>
-                                        <Field className="form-control" type="text" name="playerId" />
+                                        <Field
+                                        id="playerId"
+                                        type="text"
+                                        name="playerId"
+                                        variant="outlined"
+                                        error={this.state.errors.playerId}
+                                        helperText={
+                                        this.state.errors.playerId
+                                        ? this.state.errors.playerId
+                                        : null
+                                        }/>
                                     </fieldset>
                                     <fieldset className="form-group">
                                         <label>playerName</label>
-                                        <Field className="form-control" type="text" name="playerName" />
+                                        <Field
+                                        className="form-control"
+                                        id="playerName"
+                                        variant="outlined"
+                                        type="text"
+                                        name="playerName"
+                                        error={this.state.errors.playerName}/>
                                     </fieldset>
                                     <button className="btn btn-primary btn-details" type="submit">Save</button>
                                 </Form>
