e2d2957a900e1b1b84825fa095ce9d84da8cbb65
Armand Gaillard <armandgaillard@armands-air.home>
2020-11-29 20:42:20 +0000

    removed identation

--- src/component/PlayerComponent.jsx
+++ src/component/PlayerComponent.jsx
@@ -56,11 +56,11 @@ class PlayerComponent extends Component {
               console.log("I'm doing this playerId set")
               player.playerId = this.props.match.params.id
               TeamDataService.updatePlayer(player.playerId, player.playerName, player)
-                .then(() => this.props.history.push('/team'))
-                       .catch( error => {
-                       const response = error.response
-                       console.log("this is the errors from unitedapp ", response.data.errorDetails)
-                       this.validate(response.data.errorDetails)})
+               .then(() => this.props.history.push('/team'))
+               .catch( error => {
+               const response = error.response
+               console.log("this is the errors from unitedapp ", response.data.errorDetails)
+               this.validate(response.data.errorDetails)})
 }
 
     validate(values) {
@@ -106,45 +106,45 @@ class PlayerComponent extends Component {
                                  enableReinitialize={true}
                     >
                         {({ handleChange, touched }) => (
-                                                       <Form spacing="10">
-                                                       <div>
-                                                        <p> </p>
-                                                        </div>
-                                                       <div spacing="10">
-                                                  <TextField
-                                                        id="playerId"
-                                                        value={this.props.match.params.id}
-                                                        label="Shirt Number"
-                                                        autoComplete="off"
-                                                        inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
-                                                        name="playerId"
-                                                        error={this.state.errors.playerId && touched.playerId}
-                                                        variant="outlined"
-                                                        fullWidth
-                                                        onChange={handleChange}
-                                                        autoFocus
-                                                        helperText={
-                                                        this.state.errors.playerId && touched.playerId
-                                                        ? this.state.errors.playerId : null}
-                                                        />
-                                       </div>
-                                       <div><p> </p>
-                                       </div>
+                           <Form spacing="10">
+                           <div>
+                            <p> </p>
+                            </div>
+                           <div spacing="10">
+                              <TextField
+                                    id="playerId"
+                                    value={this.props.match.params.id}
+                                    label="Shirt Number"
+                                    autoComplete="off"
+                                    inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+                                    name="playerId"
+                                    error={this.state.errors.playerId && touched.playerId}
+                                    variant="outlined"
+                                    fullWidth
+                                    onChange={handleChange}
+                                    autoFocus
+                                    helperText={
+                                    this.state.errors.playerId && touched.playerId
+                                    ? this.state.errors.playerId : null}
+                                    />
+                                   </div>
+                                   <div><p> </p>
+                                   </div>
 
-                                        <TextField
-                                         name="playerName"
-                                          error={this.state.errors.playerName && touched.playerName}
-                                          label="Player Name"
-                                          variant="outlined"
-                                          autoComplete="off"
-                                          fullWidth
-                                          onChange={handleChange}
-                                          id="playerName"
-                                          helperText={
-                                          this.state.errors.playerName && touched.playerName
-                                          ? this.state.errors.playerName : null}
-                                          />
-                                         <div>  <p> </p> </div>
+                                <TextField
+                                 name="playerName"
+                                  error={this.state.errors.playerName && touched.playerName}
+                                  label="Player Name"
+                                  variant="outlined"
+                                  autoComplete="off"
+                                  fullWidth
+                                  onChange={handleChange}
+                                  id="playerName"
+                                  helperText={
+                                  this.state.errors.playerName && touched.playerName
+                                  ? this.state.errors.playerName : null}
+                                  />
+                                 <div>  <p> </p> </div>
 
                                     <button className="btn btn-primary btn-details" type="submit">Save</button>
                                 </Form>
--- src/component/PlayerComponentCreate.jsx
+++ src/component/PlayerComponentCreate.jsx
@@ -24,13 +24,9 @@ class PlayerComponentCreate extends Component {
            this.validate = this.validate.bind(this)
     }
 
-        componentDidMount() {
+    componentDidMount() {
         console.log("********* componentDidMount *********")
-            // eslint-disable-next-line
-
-                console.log("**** CREATE *****")
                 this.state.value = "";
-
         }
 
     onSubmit(values) {
@@ -43,39 +39,39 @@ class PlayerComponentCreate extends Component {
             playerName: values.playerName
         }
 
-                 console.log("calling team service ");
-                 TeamDataService.createPlayer(player.playerId, player.playerName, player).then(r => {
-                 console.log("response is ",r);
-                 this.props.history.push('/team')
-                 return r.team
-                 })
-                 .catch( error => {
-                 const response = error.response
-                 console.log("this is the errors from unitedapp ", response.data.errorDetails)
-                 this.validate(response.data.errorDetails)})
+         console.log("calling team service ");
+         TeamDataService.createPlayer(player.playerId, player.playerName, player).then(r => {
+         console.log("response is ",r);
+         this.props.history.push('/team')
+         return r.team
+         })
+         .catch( error => {
+         const response = error.response
+         console.log("this is the errors from unitedapp ", response.data.errorDetails)
+         this.validate(response.data.errorDetails)})
 
 }
 
     validate(values) {
 
-    this.setState({errors: ""});
-    console.log("These are the values passed in ", values);
+        this.setState({errors: ""});
+        console.log("These are the values passed in ", values);
 
-    let errors = {}
+        let errors = {}
 
-   for (let i=0; i < values.length; i++) {
-   console.log("Target from error ", values[i].target);
-   if (values[i].target === "Player Id") {
-        console.log("turning on errors for ",values[i].target)
-        errors.playerId = true;
-        errors.playerId = values[i].message;
+       for (let i=0; i < values.length; i++) {
+       console.log("Target from error ", values[i].target);
+        if (values[i].target === "Player Id") {
+            console.log("turning on errors for ",values[i].target)
+            errors.playerId = true;
+            errors.playerId = values[i].message;
        }
-   if (values[i].target === "Player Name") {
-        errors.playerName = true;
-        errors.playerName = values[i].message;
-    }
+       if (values[i].target === "Player Name") {
+            errors.playerName = true;
+            errors.playerName = values[i].message;
+        }
 
-    this.setState({errors: errors});
+        this.setState({errors: errors});
 
    }
    return errors
@@ -93,50 +89,46 @@ class PlayerComponentCreate extends Component {
                             playerId: "",
                             playerName: ""
                             }}
-                                 onSubmit={this.onSubmit}
-                                 validateOnChange={false}
-                                 validateOnBlur={false}
-                                 enableReinitialize={true}
+                             onSubmit={this.onSubmit}
+                             validateOnChange={false}
+                             validateOnBlur={false}
+                             enableReinitialize={true}
                     >
                         {({ handleChange, touched }) => (
-                                                       <Form spacing="10">
-                                                       <div>
-                                                        <p> </p>
-                                                        </div>
-                                                       <div spacing="10">
-                                                  <TextField
-                                                        id="playerId"
-                                                        label="Shirt Number"
-                                                        autoComplete="off"
-                                                        inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
-                                                        name="playerId"
-                                                        error={this.state.errors.playerId && touched.playerId}
-                                                        variant="outlined"
-                                                        fullWidth
-                                                        onChange={handleChange}
-                                                        autoFocus
-                                                        helperText={
-                                                        this.state.errors.playerId && touched.playerId
-                                                        ? this.state.errors.playerId : null}
-                                                        />
-                                       </div>
-                                       <div><p> </p>
-                                       </div>
+                               <Form spacing="10">
+                               <div> <p> </p></div>
+                               <div spacing="10">
+                                    <TextField
+                                    id="playerId"
+                                    label="Shirt Number"
+                                    autoComplete="off"
+                                    inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+                                    name="playerId"
+                                    error={this.state.errors.playerId && touched.playerId}
+                                    variant="outlined"
+                                    fullWidth
+                                    onChange={handleChange}
+                                    autoFocus
+                                    helperText={
+                                    this.state.errors.playerId && touched.playerId
+                                    ? this.state.errors.playerId : null}
+                                    />
+                                       </div><div><p> </p> </div>
 
-                                        <TextField
-                                         name="playerName"
-                                          error={this.state.errors.playerName && touched.playerName}
-                                          label="Player Name"
-                                          variant="outlined"
-                                          autoComplete="off"
-                                          fullWidth
-                                          onChange={handleChange}
-                                          id="playerName"
-                                          helperText={
-                                          this.state.errors.playerName && touched.playerName
-                                          ? this.state.errors.playerName : null}
-                                          />
-                                         <div>  <p> </p> </div>
+                                    <TextField
+                                     name="playerName"
+                                      error={this.state.errors.playerName && touched.playerName}
+                                      label="Player Name"
+                                      variant="outlined"
+                                      autoComplete="off"
+                                      fullWidth
+                                      onChange={handleChange}
+                                      id="playerName"
+                                      helperText={
+                                      this.state.errors.playerName && touched.playerName
+                                      ? this.state.errors.playerName : null}
+                                      />
+                                     <div>  <p> </p> </div>
 
                                     <button className="btn btn-primary btn-details" type="submit">Save</button>
                                 </Form>
