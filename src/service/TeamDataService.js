a81fe58672912b736f7670cac26971851fb3e9ee
Armand Gaillard <armandgaillard@armands-air.home>
2021-01-23 16:36:25 +0000

    amended reqest urls in dataservice

--- src/service/TeamDataService.js
+++ src/service/TeamDataService.js
@@ -16,21 +16,21 @@ import getEnv from '../environment';
 class TeamDataService {
 
     retrieveAllPlayers() {
-        return axios.get(`team`);
+        return axios.get(`/team`);
     }
 
     getLeague() {
-            return axios.get(`api/league`);
+            return axios.get(`/api/league`);
     }
 
     getForm() {
-            return axios.get(`api/form`);
+            return axios.get(`/api/form`);
     }
 
     getHomeForm(homeTeamId) {
             if (homeTeamId !== 'undefined') {
             console.log("************  calling API HOME FORM ************* with team id =", homeTeamId)
-            return axios.get(`api/form/${homeTeamId}`);
+            return axios.get(`/api/form/${homeTeamId}`);
             }
             else return []
     }
@@ -39,31 +39,31 @@ class TeamDataService {
 
                         if (awayTeamId !== 'undefined') {
                         console.log("************  calling API AWAY FORM ************* with team id =", awayTeamId)
-                        return axios.get(`api/form/${awayTeamId}`);
+                        return axios.get(`/api/form/${awayTeamId}`);
                         }
                         else return []
     }
 
     getNextMatch() {
-                return axios.get(`api/next`);
+                return axios.get(`/api/next`);
         }
 
     deletePlayer(id) {
-        return axios.delete(`team/${id}`);
+        return axios.delete(`/team/${id}`);
     }
 
     retrievePlayer(id) {
-        return axios.get(`team/${id}`);
+        return axios.get(`/team/${id}`);
     }
 
       updatePlayer(id, playerName, playerPosition, player) {
           console.log("Player payload is: ", player)
-          return axios.put(`team`, player);
+          return axios.put(`/team`, player);
       }
 
       createPlayer(id, playerName, playerPosition, player) {
           console.log("Player payload is: ", player)
-          return axios.post(`team`, player);
+          return axios.post(`/team`, player);
       }
 
 }
